2720a160bfa467257195021f2583e452a04cb2cc
Kunal Patel <kunalpatel@Kunals-MacBook-Pro.local>
2018-01-21 01:20:49 -0800

    don't mess it up kunal

--- app/src/main/java/com/retrospect/retrospect/TimelineUI.java
+++ app/src/main/java/com/retrospect/retrospect/TimelineUI.java
@@ -2,12 +2,45 @@ package com.retrospect.retrospect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 public class TimelineUI extends AppCompatActivity {
+
+    FloatingActionMenu fam;
+    FloatingActionButton people, event, reminder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline_ui);
+        fam = (FloatingActionMenu) findViewById(R.id.floatingActionMenu);
+        people = (FloatingActionButton) findViewById(R.id.peeps);
+        event = (FloatingActionButton) findViewById(R.id.eve);
+        reminder = (FloatingActionButton) findViewById(R.id.remi);
+
+        fam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        people.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
