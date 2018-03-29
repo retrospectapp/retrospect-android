a7a36ba4c5d33d682c8c64cf20a668d9cb2247f8
Kunal Patel <kunalpatel@Kunals-MBP.lan>
2018-03-28 19:06:14 -0700

    create reminders

--- app/src/main/java/com/retrospect/retrospect/MainActivity.java
+++ app/src/main/java/com/retrospect/retrospect/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView mainNav;
     private SlidingUpPanelLayout slidingUpPanelLayout;
     private FloatingActionMenu floatingActionMenu;
-
+    private FloatingActionButton createRemind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +26,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline_ui);
 
 
-
         mainNav = (BottomNavigationView) findViewById(R.id.main_nav);
         slidingUpPanelLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
         slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
         floatingActionMenu = (FloatingActionMenu) findViewById(R.id.floatingActionMenu);
+        createRemind = (FloatingActionButton) findViewById(R.id.remi);
+
 
 
         setFragment(new TimelineFragment());
 
+        createRemind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingActionMenu.hideMenuButton(true);
+                mainNav.setSelectedItemId(R.id.nav_reminders);
+                setFragment(new CreateReminderFragment());
+            }
+        });
         mainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.nav_reminders :
-//                        mainNav.setItemBackgroundResource(R.color.colorPrimary);
+//                      mainNav.setItemBackgroundResource(R.color.colorPrimary);
                         floatingActionMenu.hideMenuButton(true);
                         setFragment(new ReminderFragment());
                         return true;
@@ -70,9 +80,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-
 
     }
 
